5ca2aee05a849caf8daac10d442459db541bbfff
3Emme <emmettbuentiempo@gmail.com>
2020-08-17 15:41:48 -0700

    replace item array displayed with alphabeticaly ordered and capitalized copy.
    Co-authored-by: Adam Mansell <adammansellhmb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
     const item1 = $("input#item1").val();
     const item2 = $("input#item2").val();
     const item3 = $("input#item3").val();
-    const itemarray = [item1, item2, item3];
-    const alphacaparray = []
+    const itemArray = [item1, item2, item3];
+    const itemArray2 = itemArray.slice();
+    const alphaArray = itemArray2.sort();
+    let alphaCapArray = []
 
-    alphacaparray.push(itemarray[1], itemarray[0], itemarray[2]);
+    for(var i = 0 ; i < alphaArray.length ; i++){
+      alphaCapArray[i] = alphaArray[i].charAt(0).toUpperCase() + alphaArray[i].substr(1);
+  }   
 
-    itemarray.forEach(function (element) {
+    alphaCapArray.forEach(function (element) {
       $("ul#grocery-list").append('<li>' + element + '</li>');
     });
-
 
     if (!item1 || !item2 || !item3) {
       alert("Please fill out all fields. I wouldn't want you to go hungry!")
